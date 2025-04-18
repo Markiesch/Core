786ddc2a6afd44381e0045645ee9b420c9b1ef98
Markiesch <marksmeel@gmail.com>
2025-04-19 00:04:33 +0200

    refactor commandContext to record object

--- src/main/java/nl/markschuurmans/core/commands/framework/CommandContext.java
+++ src/main/java/nl/markschuurmans/core/commands/framework/CommandContext.java
@@ -3,25 +3,10 @@ package nl.markschuurmans.core.commands.framework;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CommandContext {
-    private final CommandSender sender;
-    private final String[] args;
-
-    public CommandContext(CommandSender sender, String[] args) {
-        this.sender = sender;
-        this.args = args;
-    }
-
-    public CommandSender getSender() {
-        return sender;
-    }
+public record CommandContext(CommandSender sender, String[] args) {
 
     public Player getPlayer() {
         return sender instanceof Player ? (Player) sender : null;
-    }
-
-    public String[] getArgs() {
-        return args;
     }
 
     public String getArg(int index) {
